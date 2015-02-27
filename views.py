e3f656308359f372786961411dbc7f6d979c5d79
isak.johansson <isak@k22.se>
2015-02-27 20:26:01 +0100

    changed style.css

--- views.py
+++ views.py
@@ -77,4 +77,4 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=1111)
+    app.run(debug=True, host='0.0.0.0', port=3333)
